cd2fb427072469c2bf0bd865e60032e9803fcf8b
David Nene <64870164+davidnene@users.noreply.github.com>
2024-04-02 18:17:11 +0300

    Clean code

--- pages/data.py
+++ pages/data.py
@@ -10,5 +10,5 @@ if st.checkbox("View PDFs Data Info"):
    st.dataframe(tpd_df)
 if st.checkbox("View PDFs Preprocessed Data"):
    st.dataframe(pdf_info_data)
-if st.checkbox("Embeddings Data"):
+if st.checkbox("View Embeddings Data"):
    st.dataframe(response_data[:500])
