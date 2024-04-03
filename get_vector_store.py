50fa1233783768f80e96180d89872599dc0ab698
David Nene <64870164+davidnene@users.noreply.github.com>
2024-04-03 12:54:44 +0300

    Minor fix

--- get_vector_store.py
+++ get_vector_store.py
@@ -3,11 +3,17 @@ from InstructorEmbedding import INSTRUCTOR
 from langchain.vectorstores import FAISS
 from langchain.vectorstores import Chroma
 import streamlit as st
+import shutil
+import os
 
 #creating a directory to store the vector store instance
 persist_directory = 'docs/chroma/'
 
 def get_vector_store(token_chunks):
+    chromadb_data_dir = '/docs/chroma'
+
+    if os.path.exists(chromadb_data_dir):
+        shutil.rmtree(chromadb_data_dir)
     
     embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
     # vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
