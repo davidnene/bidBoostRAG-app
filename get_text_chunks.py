04f75fc7d5c13d63f4fcd4b598b34fd19304a3d8
David Nene <64870164+davidnene@users.noreply.github.com>
2024-05-22 19:06:16 +0300

    Change chunk size

--- get_text_chunks.py
+++ get_text_chunks.py
@@ -3,8 +3,8 @@ from langchain.text_splitter import TokenTextSplitter
 
 def get_text_chunks(documents):
     token_splitter = TokenTextSplitter(
-        chunk_size = 300,
-        chunk_overlap = 50,
+        chunk_size = 1000,
+        chunk_overlap = 100,
         length_function = len
     )
 
