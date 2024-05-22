f972c512e4a035eee5d022978ca512bae51dc854
David Nene <64870164+davidnene@users.noreply.github.com>
2024-05-22 09:32:03 +0300

    Minimize chunk size

--- get_text_chunks.py
+++ get_text_chunks.py
@@ -3,8 +3,8 @@ from langchain.text_splitter import TokenTextSplitter
 
 def get_text_chunks(documents):
     token_splitter = TokenTextSplitter(
-        chunk_size = 1000,
-        chunk_overlap = 200,
+        chunk_size = 300,
+        chunk_overlap = 50,
         length_function = len
     )
 
