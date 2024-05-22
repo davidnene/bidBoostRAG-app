6056a682f87f9333ab3283aaf566c4fcc1962d7c
David Nene <64870164+davidnene@users.noreply.github.com>
2024-05-22 09:31:47 +0300

    Configure llama parse

--- get_pdf_text.py
+++ get_pdf_text.py
@@ -1,10 +1,29 @@
-import PyPDF2
-from PyPDF2 import PdfReader
-from langchain.document_loaders import PyPDFLoader
-import streamlit as st
 import os
 import re
+import nltk
+import PyPDF2
+import nest_asyncio
+import streamlit as st
+from PyPDF2 import PdfReader
+from dotenv import load_dotenv
+from llama_parse import LlamaParse
+from langchain.schema import Document
+from llama_index.core import SimpleDirectoryReader
+from langchain_community.document_loaders import UnstructuredMarkdownLoader, TextLoader
 
+
+# class CustomTextLoader(BaseLoader):
+#     def __init__(self, file_path: str, encoding: str = 'utf-8'):
+#         self.file_path = file_path
+#         self.encoding = encoding
+
+#     def load(self):
+#         with open(self.file_path, 'r', encoding=self.encoding) as f:
+#             text = f.read()
+#         # Create a Document object from the text
+#         document = Document(page_content=text)
+#         return [document]
+    
 
 @st.cache_data
 def get_pdf_text(loaded_pdfs):
@@ -12,13 +31,47 @@ def get_pdf_text(loaded_pdfs):
     documents = []
     page_contents = []
 
-#looping thru pdfs and storing content
+    load_dotenv()
+    # set up parser
+    parser = LlamaParse(
+        # api_key=llama_key,
+        result_type="markdown"  # "markdown" and "text" are available
+    )
+    
+    file_extractor = {".pdf": parser}
+    #looping thru pdfs and storing content
     for pdf in loaded_pdfs:
-        loader = PyPDFLoader(pdf)
-        pages = loader.load()
+        pages = SimpleDirectoryReader(input_files=[pdf], file_extractor=file_extractor).load_data(nest_asyncio.apply())
         pdf_name = re.search(r'[^/]+$', pdf).group(0)
         for document in pages:
             sources.append(pdf_name)
-            documents.append(document)
-            page_contents.append(document.page_content)
+            metadata = {
+                "source": f"{pdf_name}",
+                # "author": "John Doe",
+                # "title": "Example Document"
+            }
+            lp_parsed_documents = Document(page_content=document.text, metadata=metadata)
+            documents.append(lp_parsed_documents)
+            page_contents.append(document.text)
+    
+    print("Processing Markdown")
+    with open('data/output.md', 'w', encoding='utf-8') as f:  
+        for doc in documents:
+            f.write(doc.page_content + '\n')
+    
+    # with open('data/output.md', 'r', encoding='utf-8') as f:
+    #         text = f.read()
+        # Create a Document object from the text
+    # r_document = Document(page_content=text)
+    # # markdown_path = r"data\output.md"
+    # markdown_path = os.path.abspath("data/output.md")
+    # # loader = UnstructuredMarkdownLoader(markdown_path)
+    # # loader = TextLoader(markdown_path)
+    # loader = CustomTextLoader(file_path=markdown_path, encoding='utf-8')
+
+    # nltk.download('averaged_perceptron_tagger', quiet=False)
+    # print("Loading documents")
+    # # lp_parsed_documents = loader.load()
+    # lp_parsed_documents = Document(page_content=text)
+    print("Documents processed successfully!")
     return sources, documents, page_contents
