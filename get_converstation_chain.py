ba1cd9e1e4b24b40878ab66cc692cc4fa16d8bb1
David Nene <64870164+davidnene@users.noreply.github.com>
2024-05-22 18:18:21 +0300

    Minor fix

--- get_converstation_chain.py
+++ get_converstation_chain.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 
 def get_conversation_chain(vector_store):
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    llm = ChatOpenAI(model_name="gpt-4-turbo", temperature=0, max_tokens=2000)
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm = llm,
